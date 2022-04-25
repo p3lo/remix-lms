8a92a91eb2caae4e96a44b85d66c7221833cf649
p3lo <mysiacik@mysiacik.com>
2022-04-25 20:30:22 +0200

    wip profile picture edit

--- app/routes/user/__profile/profile-picture.tsx
+++ app/routes/user/__profile/profile-picture.tsx
@@ -1,5 +1,27 @@
+import { Image, Paper, Text } from '@mantine/core';
+import { useMatches } from '@remix-run/react';
+import type { User } from '~/utils/types';
+
 function ProfilePicture() {
-  return <div>ProfilePicture</div>;
+  const profile = useMatches()[0].data.profile as User;
+  return (
+    <div className="flex flex-col space-y-3">
+      <Text className="flex justify-center p-3" size="xl" weight={700}>
+        Profile picture
+      </Text>
+      <Paper
+        sx={(theme) => ({
+          backgroundColor: theme.colorScheme === 'dark' ? theme.colors.gray[9] : theme.colors.gray[2],
+        })}
+        shadow="md"
+        p="sm"
+        withBorder
+        className="w-full xs:w-3/4 md:w-2/3 mx-auto "
+      >
+        <Image fit="contain" height={300} src={profile.picture} />
+      </Paper>
+    </div>
+  );
 }
 
 export default ProfilePicture;
