34c94029b1abfed5f02589ff4031233f7a0da2e9
p3lo <mysiacik@mysiacik.com>
2022-05-06 00:12:07 +0200

    wip building db create

--- app/routes/course-builder/$slug/content/new-quiz-lesson.tsx
+++ app/routes/course-builder/$slug/content/new-quiz-lesson.tsx
@@ -1,14 +1,59 @@
-import type { LoaderFunction } from '@remix-run/node';
-import { json } from '@remix-run/node';
+import type { ActionFunction, LoaderFunction } from '@remix-run/node';
+import { redirect, json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import invariant from 'tiny-invariant';
 import CourseLessonQuizModal from '~/components/builder/CourseLessonQuizModal';
+import { prisma } from '~/utils/db.server';
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const id = url.searchParams.get('sectionId');
   invariant(id, 'sectionId id is required');
   return json({ id });
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const sectionId = formData.get('section') as string;
+  const position = formData.get('position') as string;
+  const slug = formData.get('slug') as string;
+  const duration = formData.get('duration') as string;
+  const data = formData.forEach((value, key) => {
+    if (key.startsWith('quiz-question')) {
+    }
+  });
+  const toDb = prisma.course_content_lessons.create({
+    data: {
+      sectionId: +sectionId,
+      position: +position,
+      lessonTitle: 'New Quiz Lesson',
+      type: 'quiz',
+      duration: +duration,
+      quiz: {
+        create: {
+          question: {
+            create: [
+              {
+                question: '',
+                position: 0,
+                answer: {
+                  create: [
+                    {
+                      answer: '',
+                      isCorrect: true,
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      },
+    },
+  });
+  console.log(formData);
+  return null;
+  // return redirect('/course-builder/' + slug + '/content');
 };
 
 function NewQuizLesson() {
