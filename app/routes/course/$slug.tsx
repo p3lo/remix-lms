8365fca2a7c0964b9be81806c51f8d9cdb450ec0
p3lo <mysiacik@mysiacik.com>
2022-05-16 23:16:31 +0200

    wip course details

--- app/routes/course/$slug.tsx
+++ app/routes/course/$slug.tsx
@@ -4,6 +4,7 @@ import {
   ActionIcon,
   Anchor,
   Avatar,
+  Button,
   Divider,
   List,
   Spoiler,
@@ -15,7 +16,19 @@ import {
 import type { LoaderFunction } from '@remix-run/node';
 import { json, redirect } from '@remix-run/node';
 import { Link, useLoaderData } from '@remix-run/react';
-import { RiCheckLine, RiFacebookFill, RiLinkedinFill, RiTumblrFill, RiTwitterFill } from 'react-icons/ri';
+import {
+  RiCheckLine,
+  RiFacebookFill,
+  RiLinkedinFill,
+  RiTrophyLine,
+  RiTumblrFill,
+  RiTwitterFill,
+  RiVideoLine,
+} from 'react-icons/ri';
+import { VscMortarBoard } from 'react-icons/vsc';
+import { MdOutlinePlayLesson, MdOutlineMobileFriendly, MdOutlineShoppingCart } from 'react-icons/md';
+import { IoInfiniteOutline } from 'react-icons/io5';
+import ReactPlayer from 'react-player';
 import CourseLessonList from '~/components/CourseLessonList';
 import { prisma } from '~/utils/db.server';
 import { secondsToTime, sumTime } from '~/utils/helpers';
@@ -38,6 +51,12 @@ export const loader: LoaderFunction = async ({ request }) => {
           picture: true,
           headline: true,
           bio: true,
+
+          _count: {
+            select: {
+              authored: true,
+            },
+          },
         },
       },
       whatYouLearn: true,
@@ -82,7 +101,6 @@ export const loader: LoaderFunction = async ({ request }) => {
       }
     }
   }
-
   return json({ course, sum_seconds, sum_lessons });
 };
 
@@ -102,8 +120,12 @@ function CourseItem() {
     return d.toLocaleDateString('en-UK', options);
   }
   return (
-    <div className="grid grid-cols-3">
-      <div className="col-span-2 flex flex-col space-y-5">
+    <div className="grid grid-cols-3 gap-10">
+      <div className="flex flex-col col-span-2 space-y-5">
+        <div className="flex items-center space-x-2">
+          <Text>Rating 5.00</Text>
+          <Text className="opacity-50">(2)</Text>
+        </div>
         <Title order={1}>{course.title}</Title>
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-1">
@@ -190,7 +212,7 @@ function CourseItem() {
             What Will You Learn?
           </Text>
 
-          <div className="grid grid-cols-2 text-sm space-y-0">
+          <div className="grid grid-cols-2 space-y-0 text-sm">
             {course.whatYouLearn.map((item) => (
               <List.Item icon={<RiCheckLine size={16} className="text-blue-600" />} key={item.id}>
                 {item.whatYoullLearn}
@@ -245,30 +267,115 @@ function CourseItem() {
           <Text size="xl" color="gray">
             About the instructor
           </Text>
-          <div className={`p-3 border-[0.5px] ${dark ? 'border-gray-700' : 'border-gray-300'}`}>
-            <div className="flex space-x-4 items-start">
-              <div className="flex space-x-2 items-center w-[500px]">
-                <Avatar src={course.author.picture} alt="author" radius="xl" size={45} />
-                <div className="flex flex-col">
-                  <Anchor
-                    component={Link}
-                    to={`/profile/${course.author.name}`}
-                    weight={700}
-                    size="sm"
-                    underline={false}
-                  >
-                    {course.author.name}
-                  </Anchor>
-                  <Text size="xs" color="gray" className="opacity-70 ">
-                    {course.author.headline}
+          <div className="flex flex-col">
+            <div className={`p-3 border-[0.5px] ${dark ? 'border-gray-700' : 'border-gray-300'}`}>
+              <div className="grid grid-cols-4 place-items-start">
+                <div className="flex items-center space-x-2">
+                  <Avatar src={course.author.picture} alt="author" radius="xl" size={45} />
+                  <div className="flex flex-col">
+                    <Anchor
+                      component={Link}
+                      to={`/profile/${course.author.name}`}
+                      weight={700}
+                      size="sm"
+                      underline={false}
+                    >
+                      {course.author.name}
+                    </Anchor>
+                    <Text size="xs" color="gray" className="opacity-70 ">
+                      {course.author.headline}
+                    </Text>
+                  </div>
+                </div>
+                <Spoiler className="col-span-3" maxHeight={100} showLabel="Show more" hideLabel="Hide">
+                  <TypographyStylesProvider>
+                    <div className="text-sm" dangerouslySetInnerHTML={{ __html: course.author.bio }} />
+                  </TypographyStylesProvider>
+                </Spoiler>
+              </div>
+            </div>
+            <div
+              className={`p-3 border-b-[0.5px] border-l-[0.5px] border-r-[0.5px] flex justify-between ${
+                dark ? 'border-gray-700' : 'border-gray-300'
+              }`}
+            >
+              <div></div>
+              <div className="flex items-center space-x-1">
+                <VscMortarBoard size={22} />
+                {/* @ts-ignore */}
+                <Text size="sm">{course.author._count.authored}</Text>
+                <Text size="sm" className="opacity-50">
+                  Courses
+                </Text>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className={`border-[0.5px] ${dark ? 'border-gray-700' : 'border-gray-300'}`}>
+          <div className="flex flex-col">
+            <ReactPlayer
+              config={{
+                file: {
+                  attributes: {
+                    onContextMenu: (e: Event) => e.preventDefault(),
+                    controlsList: 'nodownload',
+                  },
+                },
+              }}
+              light={course.image}
+              controls
+              width="100%"
+              height={200}
+              url={course.preview}
+            />
+            <div
+              className={`flex flex-col p-4 space-y-3 border-t-[0.5px] ${dark ? 'border-gray-700' : 'border-gray-300'}`}
+            >
+              <Text size="xl" weight={700} color="gray">
+                {`$ ${course.price}.00`}
+              </Text>
+              <div className="flex flex-col space-y-1">
+                <Text size="md" weight={500}>
+                  This course includes:
+                </Text>
+                <div className="flex items-center space-x-2">
+                  <RiVideoLine size={17} />
+                  <Text size="sm" color="gray">
+                    {secondsToTime(sum_seconds, true).split(' ')[0]} on-demand video
+                  </Text>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <MdOutlinePlayLesson size={17} />
+                  <Text size="sm" color="gray">
+                    {sum_lessons} lessons
+                  </Text>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <IoInfiniteOutline size={17} />
+                  <Text size="sm" color="gray">
+                    Full lifetime access
+                  </Text>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <MdOutlineMobileFriendly size={17} />
+                  <Text size="sm" color="gray">
+                    Access on mobile and TV
+                  </Text>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RiTrophyLine size={17} />
+                  <Text size="sm" color="gray">
+                    Certificate of completion
                   </Text>
                 </div>
               </div>
-              <Spoiler maxHeight={100} showLabel="Show more" hideLabel="Hide">
-                <TypographyStylesProvider>
-                  <div className="text-sm" dangerouslySetInnerHTML={{ __html: course.author.bio }} />
-                </TypographyStylesProvider>
-              </Spoiler>
+              <div className="flex pt-5">
+                <Button className="grow" size="lg" leftIcon={<MdOutlineShoppingCart size={24} />}>
+                  Add to cart
+                </Button>
+              </div>
             </div>
           </div>
         </div>
